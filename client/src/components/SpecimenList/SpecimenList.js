d92fa4dfdb21c81305f159c07368e58d3f818295
jonkiky <jonkiky@gmail.com>
2018-09-07 14:32:15 -0400

    [ss-1543] fix Cancer Type and Cohorts options

--- client/src/components/SpecimenList/SpecimenList.js
+++ client/src/components/SpecimenList/SpecimenList.js
@@ -107,7 +107,7 @@ class SpecimenList extends Component {
 								<span className="filter-component-input">
 									<input type="checkbox" onClick={(e) => this.props.onClick(null,allIds,e)}/>
 								</span>
-								(All Types)
+								All Types
 							</label>
 						</li>
 						{list}
