7c98a694c896eef311c127e7f246478e9d8c9752
Brian Shen <27040828+naiyume@users.noreply.github.com>
2019-08-16 11:04:59 -0400

    Made the select box get replaced by the new box when something is selected

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -775,6 +775,33 @@ class Details extends Component {
 	  }
 	
 	  createSelector(index){
+		  
+		const {items} = this.state;
+		const currItem = items[index];
+		if(currItem == "Gender"){
+		  return <div className = "select-box gray-back" ><GenderList hasUnknown={true} values={this.state.selectionList[index]} displayMax="3" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "Race"){
+		  return <div className = "select-box gray-back"><RaceList values={this.state.selectionList[index]} displayMax="3" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "Ethnicity"){
+		  return <div className = "select-box gray-back"><EthnicityList values={this.state.selectionList[index]} displayMax="3" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "Age"){
+		  return <div className = "select-box gray-back"><AgeList values={this.state.selectionList[index]} displayMax="3" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "State"){
+		  return <div className = "select-box gray-back"><DiseaseStateList values={this.state.selectionList[index]} displayMax="5" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "Categories"){
+		  return <div className = "select-box gray-back"><CollectedDataList values={this.state.selectionList[index]} displayMax="5" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "Biospecimen"){
+		  return <div className = "select-box gray-back"><CollectedSpecimensList values={this.state.selectionList[index]} displayMax="5" onClick={v => this.handleGeneralListClick(v, index)} startOpen={true}/></div>;
+		}
+		else if(currItem == "Cancers"){
+		  return <div className = "select-box gray-back"><CollectedCancersList hasNoCancer={false} title="Cancers Collected" innertitle="Cancers Collected"  hasSelectAll={true} values={this.state.selectionList[index]} displayMax="5" onClick={(v,allIds,e) => this.handleAdvancedCancerClick(v, allIds, e, index)} startOpen={true}/></div>;
+		}
 		  if(index > 0){
 			return <select className="type-selector" value={this.state.items[index]} onChange={e => this.handleSelectChange(e,index)}>
 			  <option value="Select" selected disabled hidden>-Select Term-</option>
@@ -860,6 +887,7 @@ class Details extends Component {
 			)
 		} 
 		else{
+			//{this.createSelectItems(index)}
 			const { items, itemText } = this.state;
 			const itemList = items.map((item, index) => (
 			<div>
@@ -868,7 +896,7 @@ class Details extends Component {
 						{this.createSelector(index)}
 						<button className="add-button"onClick={e => this.addItem(index+1)}>+</button>
 						<button className="remove-button" onClick={e => this.removeItem(index)}>&times;</button>
-						{this.createSelectItems(index)}
+						
 					</div>
 			</div>
 			));
