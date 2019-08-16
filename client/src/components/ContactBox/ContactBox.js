14a604266718dab65f5597e6c52a7a16a5ea152b
Brian Shen <27040828+naiyume@users.noreply.github.com>
2019-08-16 16:35:54 -0400

    Added the validation to the contact box

--- client/src/components/ContactBox/ContactBox.js
+++ client/src/components/ContactBox/ContactBox.js
@@ -60,7 +60,30 @@ class ContactBox extends Component {
       state.org_required = state.organization.trim() === "";
       state.email_required = state.email.trim() === "";
       state.message_required = state.message.trim() === "";
-      if(state.firstname_required || state.lastname_required || state.org_required || state.email_required || state.message_required){
+      
+      //Check validity of email and phone number
+      if(state.email.trim().search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) === -1){
+
+        state.email_invalid = true;
+
+      }
+      else{
+        state.email_invalid = false;
+      }
+
+      //Check validity of phone number if one is given
+      //if(state.phone.trim() !== "" && state.phone.trim().search(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/) === -1){
+      
+
+      if(state.phone.trim().search(/^[+]?([0-9]|[-])*$/) === -1){
+
+        state.phone_invalid = true;
+      }
+      else{
+        state.phone_invalid = false;
+      }
+
+      if(state.firstname_required || state.lastname_required || state.org_required || state.email_required || state.message_required || state.email_invalid || state.phone_invalid){
         this.setState(state);
       }
       else{
@@ -89,11 +112,14 @@ class ContactBox extends Component {
           setTimeout(() =>{
             this.setState({
               submitted:!this.state.submitted,
+              background_gray:true,
               firstname_required:false,
               lastname_required:false,
               org_required:false,
               email_required:false,
               message_required:false,
+              email_invalid:false,
+              phone_invalid:false,
               firstname:"",
               lastname:"",
               organization:"",
@@ -104,7 +130,8 @@ class ContactBox extends Component {
             });
           }, 1500);
         });
-      }
+            }
+            
     }
 
   render() {
@@ -117,9 +144,11 @@ class ContactBox extends Component {
     let firstname_cls = this.state.firstname_required?"contact-us-field field-required":"contact-us-field";
     let lastname_cls = this.state.lastname_required?"contact-us-field field-required":"contact-us-field";
     let org_cls = this.state.org_required?"contact-us-field field-required":"contact-us-field";
-    let email_cls = this.state.email_required?"contact-us-field field-required":"contact-us-field";
+    let email_cls = this.state.email_required?"contact-us-field field-required":(this.state.email_invalid?"contact-us-field field-invalid":"contact-us-field");
     let message_cls = this.state.message_required?"contact-us-field field-required":"contact-us-field";
-    const submit_cls = this.state.submitted?"message-top fade-away":"message-top";
+    let phone_cls = this.state.phone_invalid?"contact-us-field field-invalid":"contact-us-field"
+    const submit_cls = this.state.background_gray?"message-top fade-away":"message-top";
+    const success_back = this.state.background_gray?"modal":"non-modal";
     return (
       <div>
         <div id="contactFlag" className="rightSideFlag" data-toggle="modal" data-target="#contactOverlay">
