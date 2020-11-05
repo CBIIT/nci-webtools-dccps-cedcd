3361ced1bbc93e478714a75f23a9ccd05ee189c7
Chao Zhang <chao.zhang3@nih.gov>
2020-11-05 12:51:19 -0500

    db deployment test 2020-11-05

---
+++ database/deploy_scripts/cedcd_deploy_1.sql
@@ -0,0 +1,15 @@
+-- MySQL Distrib 8
+--
+--   Database: cedcd
+-- ------------------------------------------------------
+-- Server version	8.0
+-- revised according to questionnarire v6 in 2020 =====
+--  the order is not strictly alphabetical order, 
+--  it is considering relationship constraints
+
+-- test 
+
+DROP TABLE IF EXISTS `test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test` ( test varchar(50)) ENGINE = InnoDB DEFAULT CHARSET = utf8;
