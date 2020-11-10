d37f88df69da4a692bbd4523b39ff6fb4fb3457c
Chao Zhang <chao.zhang3@nih.gov>
2020-11-10 16:42:38 -0500

    db schema revised for specimen and collected data types list v2

--- client/src/components/Admin/ManageCohort.js
+++ client/src/components/Admin/ManageCohort.js
@@ -26,7 +26,7 @@ class ManageCohort extends Component {
 				column: "acronym",
 				order: "asc"
 			},
-			pageInfo: { page: 1, pageSize: 10, total: 0 },
+			pageInfo: { page: 1, pageSize: 15, total: 0 },
 			lastPage: 1,
 		};
 		this.toFocus = React.createRef()
@@ -329,92 +329,92 @@ class ManageCohort extends Component {
 		}
 
 		return <UserSessionContext.Consumer>
-		{userSession => (
-			!(process.env.NODE_ENV === 'development2' || (userSession && userSession.role === 'SystemAdmin')) &&
-			<Unauthorized /> ||
-			<div>
-				<h1 className="welcome pg-title">Manage Cohorts</h1>
-				<p className="welcome">Browse the list of cohorts or use the filter options to search cohorts according to cohort status.
-				Then select the cohorts about which one you'd like to see the details or select the proper action to take on the cohort.
+			{userSession => (
+				!(process.env.NODE_ENV === 'development2' || (userSession && userSession.role === 'SystemAdmin')) &&
+				<Unauthorized /> ||
+				<div>
+					<h1 className="welcome pg-title">Manage Cohorts</h1>
+					<p className="welcome">Browse the list of cohorts or use the filter options to search cohorts according to cohort status.
+					Then select the cohorts about which one you'd like to see the details or select the proper action to take on the cohort.
       		    </p>
-				<div className="filter-block col-md-12">
-					<div className="col-sm-4 ">
-						<div class="form-group has-feedback has-search">
-							<span class="glyphicon glyphicon-search form-control-feedback"></span>
-							<input type="text" class="form-control" value={this.state.filter.cohortSearch} placeholder="Search with key word " onChange={(e) => this.handleCohortSearchChange(e)} />
+					<div className="filter-block col-md-12">
+						<div className="col-sm-4 ">
+							<div class="form-group has-feedback has-search">
+								<span class="glyphicon glyphicon-search form-control-feedback"></span>
+								<input type="text" class="form-control" value={this.state.filter.cohortSearch} placeholder="Search with key word " onChange={(e) => this.handleCohortSearchChange(e)} />
+							</div>
+						</div>
+						<div className="col-sm-3 ">
+							<div id="cohortstatus" className="filter-component">
+								<CohortStatusList hasUnknown={true} values={this.state.filter.cohortstatus} displayMax="3" onClick={this.handleCohortStatusClick} />
+							</div>
+						</div>
+						<div className="col-sm-3 ">
+							<a id="filterClear" className="btn-filter" href="javascript:void(0);" onClick={this.clearFilter}>
+								<i className="fas fa-times"></i> Clear All </a>
+
 						</div>
 					</div>
-					<div className="col-sm-3 ">
-						<div id="cohortstatus" className="filter-component">
-							<CohortStatusList hasUnknown={true} values={this.state.filter.cohortstatus} displayMax="3" onClick={this.handleCohortStatusClick} />
-						</div>
-					</div>
-					<div className="col-sm-3 ">
-						<a id="filterClear" className="btn-filter" href="javascript:void(0);" onClick={this.clearFilter}>
-							<i className="fas fa-times"></i> Clear All </a>
-
-					</div>
-				</div>
-				<div className="filter-block home col-md-12">
-					<div className="row" style={{ "display": "flex" }}>
-						<div style={{ "marginLeft": "auto", "paddingLeft": "10px", "paddingRight": "1rem", "position": "relative", "paddingTop": "7px" }}>
-							<PageSummary pageInfo={this.state.pageInfo} mid="true" />
-						</div>
-						<div style={{ "paddingRight": "15px", "paddingTop": "5px" }}>
-							<Paging pageInfo={this.state.pageInfo} onClick={(i) => this.gotoPage(i)} />
-						</div>
-					</div>
-				</div>
-
-				<div className="table-inner col-md-12">
-					<div className="cedcd-table home">
-						<div>
-							<table cellSpacing="0" cellPadding="5" useaccessibleheaders="true" showheaders="true" id="cohortGridView" >
-								<thead>
-									<tr id="summaryHeader" className="col-header">
-										{this.renderTableHeader("name", "30%")}
-										{this.renderTableHeader("acronym", "10%")}
-										{this.renderTableHeader("status", "15%")}
-										{this.renderTableHeader("create_by", "20%")}
-										{this.renderTableHeader("update_time", "15%")}
-										{this.renderTableHeader("action", "10%")}
-									</tr>
-								</thead>
-								<tbody>
-									{content}
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>
-
-				<div className="filter-block home col-md-12">
-
-					<div className="row" style={{ "display": "flex" }}>
-						<div class="pageSize">
-							Page Size: <select name="pageSizeSelect" value={this.state.pageInfo.pageSize} onChange={(e) => this.handleCohortPageSizeChange(e)} >
-								<option>Page Size</option>
-								<option value="5">5</option>
-								<option value="10">10</option>
-								<option value="15">15</option>
-								<option value="20">20</option>
-							</select>
-						</div>
-						<div style={{ "marginLeft": "auto", "paddingRight": "1rem", "paddingTop": "7px" }}>
-
-							<div>
+					<div className="filter-block home col-md-12">
+						<div className="row" style={{ "display": "flex" }}>
+							<div style={{ "marginLeft": "auto", "paddingLeft": "10px", "paddingRight": "1rem", "position": "relative", "paddingTop": "7px" }}>
 								<PageSummary pageInfo={this.state.pageInfo} mid="true" />
 							</div>
-
+							<div style={{ "paddingRight": "15px", "paddingTop": "5px" }}>
+								<Paging pageInfo={this.state.pageInfo} onClick={(i) => this.gotoPage(i)} />
+							</div>
 						</div>
+					</div>
 
-						<div style={{ "paddingRight": "15px", "paddingTop": "5px" }}>
-							<Paging pageInfo={this.state.pageInfo} onClick={(i) => this.gotoPage(i)} />
+					<div className="table-inner col-md-12">
+						<div className="cedcd-table home">
+							<div>
+								<table cellSpacing="0" cellPadding="5" useaccessibleheaders="true" showheaders="true" id="cohortGridView" >
+									<thead>
+										<tr id="summaryHeader" className="col-header">
+											{this.renderTableHeader("name", "30%")}
+											{this.renderTableHeader("acronym", "10%")}
+											{this.renderTableHeader("status", "15%")}
+											{this.renderTableHeader("create_by", "20%")}
+											{this.renderTableHeader("update_time", "15%")}
+											{this.renderTableHeader("action", "10%")}
+										</tr>
+									</thead>
+									<tbody>
+										{content}
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+
+					<div className="filter-block home col-md-12">
+
+						<div className="row" style={{ "display": "flex" }}>
+							<div class="pageSize">
+								Page Size: <select name="pageSizeSelect" value={this.state.pageInfo.pageSize} onChange={(e) => this.handleCohortPageSizeChange(e)} >
+									<option>Page Size</option>
+									<option value="5">5</option>
+									<option value="10">10</option>
+									<option value="15">15</option>
+									<option value="20">20</option>
+								</select>
+							</div>
+							<div style={{ "marginLeft": "auto", "paddingRight": "1rem", "paddingTop": "7px" }}>
+
+								<div>
+									<PageSummary pageInfo={this.state.pageInfo} mid="true" />
+								</div>
+
+							</div>
+
+							<div style={{ "paddingRight": "15px", "paddingTop": "5px" }}>
+								<Paging pageInfo={this.state.pageInfo} onClick={(i) => this.gotoPage(i)} />
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		)}</UserSessionContext.Consumer>;
+			)}</UserSessionContext.Consumer>;
 	}
 }
 
