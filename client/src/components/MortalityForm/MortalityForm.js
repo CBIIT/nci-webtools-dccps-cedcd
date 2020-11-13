d1fed98ff8e5c1ea1bcf5b0f004c4b1a517303e7
bchen49 <benchen4998@gmail.com>
2020-11-13 10:54:28 -0500

    add E.2. to cohort form

--- client/src/components/MortalityForm/MortalityForm.js
+++ client/src/components/MortalityForm/MortalityForm.js
@@ -8,17 +8,27 @@ const MortalityForm = ({ ...props }) => {
     return <div className='col-md-12' style={{ marginTop: '20px', paddingLeft: '0px' }}>
 
         <div className='form-group col-md-12'>
-            <label htmlFor='mortalityYear' className='col-md-5'>E.1 Most recent year of mortality follow up<span style={{ color: 'red' }}>*</span></label>
+            <label htmlFor='mortalityYear' className='col-md-4' style={{lineHeight: '2em'}}>E.1 Most recent year of mortality follow up<span style={{ color: 'red' }}>*</span></label>
             <span className="col-md-3">
                 <input name='mortalityYear' className='form-control' placeholder='yyyy' />
             </span>
         </div>
-        <div className='form-group col-md-12' style={{marginTop:'10px'}}>
-            <label htmlFor='mortalityYear' className='col-md-5'>E.2 How did your cohort confirm death? (select all that apply)<span style={{ color: 'red' }}>*</span></label>
-            <div className="col-md-4">
-                
-            </div>
+        <div className='form-group col-md-12' style={{ marginTop: '10px' }}>
+            <label htmlFor='confirmDeath' className='col-md-5'>E.2 How did your cohort confirm death? (select all that apply)<span style={{ color: 'red' }}>*</span></label>
         </div>
+
+        <div className="form-group col-md-12">
+            <div htmlFor="deathIndex" className='col-md-4'>U.S. National Death Index (NDI) linkage</div>
+            <span className='col-md-1' style={{paddingRight: '0', marginRight: '0'}}>
+                <input type='checkbox' name='deathIndexNo' style={{width:'40px'}}/>
+                <span>No</span>
+            </span>
+            
+            <span className="col-md-2">
+
+            </span>
+        </div>
+
 
     </div>
 }
