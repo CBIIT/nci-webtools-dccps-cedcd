3554f7c54cf58297f2cf3f93f4364ceadf7aef9d
Chao Zhang <chao.zhang3@nih.gov>
2020-11-24 23:01:54 -0500

    include db procedures merge issue updates

--- database/deploy_scripts/rename_lu_table_names.sql
+++ database/deploy_scripts/rename_lu_table_names.sql
@@ -5,3 +5,7 @@
 
 ALTER TABLE lu_data_collected_category RENAME TO lu_data_category ;
 ALTER TABLE lu_category RENAME TO  lu_person_category ;
+
+insert into lu_data_category (id,category) values (99 ,'Cancer Treatment');
+
+UPDATE lu_data_category SET category= 'Physical function measures' WHERE id= 41;
