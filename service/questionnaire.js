e300258b28eea203fd5088ee4a9f1619745a1db4
Brian Park <park-brian@users.noreply.github.com>
2020-11-25 08:40:12 -0500

    add backend routes for querying service

--- service/questionnaire.js
+++ service/questionnaire.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('../components/mysql');
 var logger = require('../components/logger');
+var cache = require('../components/cache');
 var fs = require('fs')
 
 router.post('/upload/:id/:category', async function(req, res, next) {
@@ -171,6 +172,125 @@ router.post('/update_mortality/:id', function(req,res){
         else
             res.json({status:500, message:'update failed'})
     })
-})
+});
+
+router.get('/cancer_count', async (request, response) => {
+    const { mysql } = request.app.locals;
+    try {
+        const records = await mysql.query(
+            `SELECT * FROM cancer_count WHERE cohort_id = ?`,
+            request.query.cohort_id
+        );
+        response.json(records);
+    }
+    catch (e) {
+        logger.error(e);
+        response.status(500).json({message: 'could not fetch cancer_info'})
+    }
+});
+
+router.post('/cancer_count', async (request, response) => {
+    const columns = [
+        'cohort_id',
+        'cancer_id',
+        'gender_id',
+        'case_type_id',
+        'cancer_counts',
+    ];
+
+    try {
+        for (const record of request.body)
+            await mysql.upsert({table: 'cancer_count', columns, record});
+        response.json({status:200, message: 'update successful'});
+    } catch (e) {
+        logger.error(e);
+        response.json({status:500, message: 'update failed'});
+    }
+});
+
+router.get('/cancer_info', async (request, response) => {
+    const { mysql } = request.app.locals;
+    try {
+        const records = await mysql.query(
+            `SELECT * FROM cancer_info WHERE cohort_id = ?`,
+            request.query.cohort_id
+        );
+        response.json(records.length ? records[0] : null);
+    }
+    catch (e) {
+        logger.error(e);
+        response.status(500).json({message: 'could not fetch cancer_info'})
+    }
+});
+
+router.post('/cancer_info', async (request, response) => {
+    const columns = [
+        'cohort_id',
+        'ci_confirmed_cancer_year',
+        'ci_ascertained_self_reporting',
+        'ci_ascertained_tumor_registry',
+        'ci_ascertained_medical_records',
+        'ci_ascertained_other',
+        'ci_ascertained_other_specify',
+        'ci_cancer_recurrence',
+        'ci_second_primary_diagnosis',
+        'ci_cancer_treatment_data',
+        'ci_treatment_data_surgery',
+        'ci_treatment_data_radiation',
+        'ci_treatment_data_chemotherapy',
+        'ci_treatment_data_hormonal_therapy',
+        'ci_treatment_data_bone_stem_cell',
+        'ci_treatment_data_other',
+        'ci_treatment_data_other_specify',
+        'ci_data_source_admin_claims',
+        'ci_data_source_electronic_records',
+        'ci_data_source_chart_abstraction',
+        'ci_data_source_patient_reported',
+        'ci_data_source_other',
+        'ci_data_source_other_specify',
+        'ci_collect_other_information',
+        'ci_cancer_staging_data',
+        'ci_tumor_grade_data',
+        'ci_tumor_genetic_markers_data',
+        'ci_tumor_genetic_markers_data_describe',
+        'ci_histologically_confirmed',
+        'ci_cancer_subtype_histological',
+        'ci_cancer_subtype_molecular',
+        'mdc_acute_treatment_toxicity',
+        'mdc_late_effects_of_treatment',
+        'mdc_symptoms_management',
+        'mdc_other_cancer_condition',
+        'mdc_other_cancer_condition_specify',
+    ];
+
+    try {
+        await mysql.upsert({table: 'cancer_info', columns, record: request.body});
+        response.json({status:200, message: 'update successful'});
+    } catch (e) {
+        logger.error(e);
+        response.json({status:500, message: 'update failed'});
+    }
+});
+
+router.get('/lookup', async (request, response) => {
+    let { locals } = request.app;
+    let { lookup, mysql } = locals;
+    
+    if (!lookup) {
+        locals.lookup = lookup = {
+            cancer: await mysql.query(`SELECT id as value, icd9, icd10, cancer FROM lu_cancer ORDER BY icd9 = ''`),
+            case_type: await mysql.query(`SELECT id as value, case_type as label FROM lu_case_type`),
+            category: await mysql.query(`SELECT id as value, category as label FROM lu_category`),
+            cohort_status: await mysql.query(`SELECT id as value, cohortstatus as label FROM lu_cohort_status`),
+            data_collected_category: await mysql.query(`SELECT id as value, category, sub_category FROM lu_data_collected_category`),
+            ethnicity: await mysql.query(`SELECT id as value, ethnicity as label FROM lu_ethnicity`),
+            gender: await mysql.query(`SELECT id as value, gender as label FROM lu_gender`),
+            race: await mysql.query(`SELECT id as value, race as label FROM lu_race`),
+            specimen: await mysql.query(`SELECT id as value, specimen as label, sub_category FROM lu_specimen`),
+        }
+    }
+
+    response.json(lookup);
+});
 
 module.exports = router
