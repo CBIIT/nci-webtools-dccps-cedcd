14667dd63e1e0d76452815a4dffc665d755ed3a4
Brian Park <park-brian@users.noreply.github.com>
2020-11-27 04:11:26 -0500

    update upsert function

--- components/mysql.js
+++ components/mysql.js
@@ -251,22 +251,41 @@ function deferUntilConnected(connection) {
 	});
 }
 
-function upsert({connection, table, columns, record}) {
-	if (!connection) connection = pool;
-	if (!columns) columns = Object.keys(record);
+async function upsert({connection, table, record}) {
+	for (let key in record)
+		if (record[key] === undefined)
+			delete record[key];
 
+	if (!connection) connection = pool;
+	const query = promisify(connection.query).bind(connection);
+	const escape = connection.escapeId.bind(connection);
+
+	const validColumns = (await query(
+		`SELECT COLUMN_NAME FROM information_schema.COLUMNS where TABLE_NAME = ?`,
+		table
+	)).map(e => e.COLUMN_NAME);
+	
+	if (record.id) {
+		// when upserting, columns without default values must be provided
+		const [existingRecord] = await query(
+			`SELECT * FROM ?? WHERE id = ?`,
+			[table, record.id]
+		);
+		record = {...existingRecord, ...record};
+	}
+
+	const columns = Object.keys(record).filter(column => validColumns.includes(column));
 	const values = columns.map(column => record[column]);
 	const valuePlaceholders = columns.map(_ => '?').join(',');
-	const valueAssignments = columns.map(column => 
-		`${connection.escapeId(column)} = VALUES(${connection.escapeId(column)})`
+	const valueAssignments = validColumns.map(column => 
+		`${escape(column)} = COALESCE(VALUES(${escape(column)}), ${escape(column)}) `
 	).join(',');
 
-	// return promise
-	const query = promisify(connection.query).bind(connection);
+
 
 	// if upserting, ensure that a unique key exists for the specified columns
 	// and that non-null values do not exist in this unique key
-	return query(
+	return await query(
 		`INSERT INTO ?? (??) VALUES (${valuePlaceholders})
 		ON DUPLICATE KEY UPDATE ${valueAssignments}`, 
 		[table, columns, ...values]
