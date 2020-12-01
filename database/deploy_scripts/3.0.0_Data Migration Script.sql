114b4aaee5dc560ab9f75f8a8704f947dd5ca734
Chao Zhang <chao.zhang3@nih.gov>
2020-12-01 14:32:21 -0500

    clean up db scripts, create 3.0.0_Update.sql

--- database/deploy_scripts/3.0.0_Data Migration Script.sql
+++ database/deploy_scripts/3.0.0_Data Migration Script.sql
@@ -1780,4 +1780,55 @@ select technology_id,
   tech_use_of_cloud_describe
 from cedcd.cohort_technology;
 
+/*
+*  update published cohort status 
+*/
+
+DROP PROCEDURE IF EXISTS `update_cohort_published_status`;
+
+DELIMITER //
+
+CREATE PROCEDURE `update_cohort_published_status`()
+BEGIN
+  DECLARE bDone INT;
+
+  DECLARE var1 INT;    -- or approriate type
+ 
+  DECLARE curs CURSOR FOR  SELECT id FROM cohort WHERE status ='published'; 
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET bDone = 1;
+
+  OPEN curs;
+
+  SET bDone = 0;
+  REPEAT
+    FETCH curs INTO var1;
+    
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'A', 'complete')
+    on duplicate key update status  = 'complete';
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'B', 'complete')
+    on duplicate key update status  = 'complete';
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'C', 'complete')
+    on duplicate key update status  = 'complete';
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'D', 'complete')
+    on duplicate key update status  = 'complete';
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'E', 'complete')
+    on duplicate key update status  = 'complete';
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'F', 'complete')
+    on duplicate key update status  = 'complete';
+    INSERT INTO cohort_edit_status (cohort_id,page_code, status) VALUES (var1, 'G', 'complete')
+    on duplicate key update status  = 'complete';
+    
+  UNTIL bDone END REPEAT;
+
+  CLOSE curs;
+  
+END //
+
+DELIMITER ;
+
+call update_cohort_published_status();
+
+DROP PROCEDURE IF EXISTS `update_cohort_published_status`;
+
+
 -- ====== End of Migration script =========
--- database/deploy_scripts/update_user_table.sql
+++
@@ -1,1 +0,0 @@
-alter table user add user_name varchar(200) null unique after id;
