3c4181eb5696961ed246238aef2f34c049c5f7db
Brian Park <park-brian@users.noreply.github.com>
2020-12-14 13:10:35 -0500

    reword authorization message

--- client/src/components/Unauthorized/Unauthorized.js
+++ client/src/components/Unauthorized/Unauthorized.js
@@ -4,7 +4,7 @@ export default function Unauthorized(props) {
     return <>
         <div className="text-center">
             <h1>Unauthorized</h1>
-            <p>You are not authorized to view this resource. Please log in as a user with the appropriate authorization. </p>
+            <p>You are not authorized to view this resource. Please log in as an authorized user. </p>
         </div>
     </>
 }
