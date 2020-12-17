1d59b422c6e218f1592b02e2a66674a4b5e83b83
Zhao <zhaox18@nih.gov>
2020-12-17 09:33:49 -0500

    fixed bugs

--- client/src/validators/medianAgeValidator.js
+++ client/src/validators/medianAgeValidator.js
@@ -1,9 +1,9 @@
 const medianAgeValidator = (value, isRequired, min, max) => {
-    if(isRequired && !value) return 'Please provide a value'
+    if(isRequired && !value) return 'Required Filed'
     else if(!/^\s*[1-9][0-9][0-9]?\s*$/.test(value)) return 'Invalid age'
     else{
         if(parseInt(value) < parseInt(min) || parseInt(value) > parseInt(max))
-            return 'Out of age range'
+            return 'Out Of Age Range'
     }
 }
 
