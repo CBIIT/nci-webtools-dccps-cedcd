291e77d267a3a2761adeb23a4d65b8a803379b40
Brian Park <park-brian@users.noreply.github.com>
2020-12-23 04:40:21 -0500

    add disabled prop

--- client/src/components/Tooltip/Tooltip.js
+++ client/src/components/Tooltip/Tooltip.js
@@ -2,32 +2,34 @@ import React from 'react'
 import {
     createMuiTheme,
     MuiThemeProvider
-  } from "@material-ui/core/styles";
+} from "@material-ui/core/styles";
 import Tooltip from '@material-ui/core/Tooltip'
 import Zoom from '@material-ui/core/Zoom';
 
 const defaultTheme = createMuiTheme();
 const theme = createMuiTheme({
-overrides: {
-    MuiTooltip: {
-      tooltip: {
-        fontSize: '13px',
-        color: 'white',
-        backgroundColor: "red"
+    overrides: {
+        MuiTooltip: {
+            tooltip: {
+                fontSize: '13px',
+                color: 'white',
+                backgroundColor: "red"
+            }
+        }
     }
-    }
-}
 });
 
 
-const Reminder = ({...props}) => {
-    return  <MuiThemeProvider theme={defaultTheme}>
-        <MuiThemeProvider theme={theme}>
-            <Tooltip title={props.message} TransitionComponent={Zoom} {...props}>
-            {props.children}
-            </Tooltip>
+const Reminder = ({ ...props }) => {
+    return props.tooltipDisabled
+        ? props.children
+        : <MuiThemeProvider theme={defaultTheme}>
+            <MuiThemeProvider theme={theme}>
+                <Tooltip title={props.message} TransitionComponent={Zoom} {...props}>
+                    {props.children}
+                </Tooltip>
+            </MuiThemeProvider>
         </MuiThemeProvider>
-    </MuiThemeProvider>
 }
 
 export default Reminder
