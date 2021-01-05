3c54589c9743d878e6f51fddd83841e8c24cf3a2
Brian Park <park-brian@users.noreply.github.com>
2021-01-05 11:56:03 -0500

    set session maxAge to 15 minutes

--- service/session.js
+++ service/session.js
@@ -1,11 +1,11 @@
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
-const maxSessionAge = 86400000;
+const maxSessionAge = 1000 * 60 * 15; // 15 minutes
 
 module.exports = session({
     cookie: { maxAge: maxSessionAge },
     store: new MemoryStore({
-      checkPeriod: maxSessionAge
+      checkPeriod: maxSessionAge / 10
     }),
     resave: false,
     saveUninitialized: true,
