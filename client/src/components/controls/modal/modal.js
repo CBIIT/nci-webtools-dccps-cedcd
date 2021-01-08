ac0d04fe217320b121a2e8923535bdad34624f35
Brian Park <park-brian@users.noreply.github.com>
2021-01-08 00:15:29 -0500

    add static backdrop to modal

--- client/src/components/controls/modal/modal.js
+++ client/src/components/controls/modal/modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 const CenterModal = (props) => {
   return (
-    <Modal show={props.show} >
+    <Modal show={props.show} backdrop="static">
       <Modal.Header>
         <Modal.Title>
           { props.title || 
