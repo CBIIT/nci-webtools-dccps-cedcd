25b1ba0381bfaabfe2ee4d262a0427be26da4f2c
Brian Park <park-brian@users.noreply.github.com>
2021-01-12 16:10:13 -0500

    keep cohort id in sync with url

--- client/src/components/Questionnaire/Questionnaire.js
+++ client/src/components/Questionnaire/Questionnaire.js
@@ -6,13 +6,20 @@ import { fetchCohort } from '../../reducers/cohort';
 import { updateUserSession } from '../../reducers/user';
 import { initializeLookup } from '../../reducers/lookupReducer';
 import SelectCohort from '../SelectCohort/SelectCohort';
+import allactions from '../../actions'
 
 const Questionnaire = ({ ...props }) => {
     const dispatch = useDispatch();
-    const cohortID = useSelector(state => state.cohortIDReducer) || window.location.pathname.split('/').pop();
+    const cohortID = useSelector(state => +state.cohortIDReducer);
     const userSession = useSelector(state => state.user);
     const isAuthorized = userSession && (userSession.role === 'CohortAdmin' || userSession.role === 'SystemAdmin');
     const hasAccess = userSession && (userSession.role === 'SystemAdmin' || (userSession.cohorts || []).map(c => +c.id).includes(+cohortID));
+
+    useEffect(() => {
+        let id = +window.location.pathname.split('/').pop();
+        if (!cohortID || id !== cohortID)
+            dispatch(allactions.cohortIDAction.setCohortId(id));
+    }, []);
 
     useEffect(() => { 
         if (cohortID) {
