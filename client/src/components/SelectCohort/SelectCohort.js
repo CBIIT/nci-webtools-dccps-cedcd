d2e59ce51955de94c4609a1917e77330ccefeeb7
Brian Park <park-brian@users.noreply.github.com>
2021-01-13 01:25:28 -0500

    use history hook

--- client/src/components/SelectCohort/SelectCohort.js
+++ client/src/components/SelectCohort/SelectCohort.js
@@ -1,30 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import RequireAuthorization from '../RequireAuthorization/RequireAuthorization';
 import Select from 'react-select';
-import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 export default function SelectCohort() {
-    const [cohort, setCohort] = useState(null)
     const user = useSelector(state => state.user);
-
-    const handleChange = (event) => {
-        setCohort(event.label)
-        window.location.assign(window.location.origin + '/cohort/questionnaire/' + event.value)
-    }
+    const history = useHistory();
 
     return <RequireAuthorization role="CohortAdmin">
         <div>
             <h1 className="welcome pg-title">Select a Cohort</h1>
-            <p class="welcome">
+            <p className="welcome">
                 Please select the cohort you wish to update from the list below.
-                </p>
+            </p>
 
-            <div className='col-md-12'>
+            <div className="col-md-12">
                 <Select
-                    name='cohorts'
-                    options={user.cohorts.map(cohort => ({ label: cohort.acronym + ' - ' + cohort.name, value: cohort.id }))}
-                    onChange={handleChange}
+                    options={user.cohorts.map(({acronym, name, id}) => ({ label: `${acronym} - ${name}`, value: id }))}
+                    onChange={e => history.push(`/cohort/questionnaire/${e.value}`)}
                 />
             </div>
         </div>
