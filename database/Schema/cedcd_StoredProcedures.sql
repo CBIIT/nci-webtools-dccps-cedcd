eec34c938b3b6c27d0499c5f42cb0274d59e2b9e
Brian Park <park-brian@users.noreply.github.com>
2021-01-14 20:24:16 -0500

    do not consider archived cohorts

--- database/Schema/cedcd_StoredProcedures.sql
+++ database/Schema/cedcd_StoredProcedures.sql
@@ -2378,7 +2378,7 @@ CREATE PROCEDURE `select_unpublished_cohort_id`(in targetID int, out new_id int)
 BEGIN
 	  set new_id = targetID; -- assume it is draft
     if exists (select * from cohort where status = 'published' and id = targetID) then -- if it is published
-        if exists (select * from cohort a join cohort b on a.acronym = b.acronym and a.status <> b.status and b.id = targetID) then -- find its copy
+        if exists (select * from cohort a join cohort b on a.acronym = b.acronym and a.status <> b.status and b.id = targetID and b.status != 'archived') then -- find its copy
             select a.id into new_id from cohort a join cohort b on a.acronym = b.acronym and a.status <> b.status and b.id = targetID;
         else -- if copy not exists, create a new one
            insert cohort (name, acronym, status, publish_by, document_ver, create_time, update_time) select name, acronym, 'draft', null,'8.1', now(), now() from cohort
