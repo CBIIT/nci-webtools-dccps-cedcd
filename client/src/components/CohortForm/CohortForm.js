f0c02ddf5948030fe221fa5bc82f8a8389cd4e21
Zhao <zhaox18@nih.gov>
2021-01-25 14:19:54 -0500

    remove redundant line

--- client/src/components/CohortForm/CohortForm.js
+++ client/src/components/CohortForm/CohortForm.js
@@ -870,8 +870,8 @@ const CohortForm = ({ ...props }) => {
 
     return (
         <Container>
-            {successMsg && <Messenger message='update succeeded' severity='success' open={true} changeMessage={setSuccessMsg} />}
-            {failureMsg && <Messenger message='update failed' severity='warning' open={true} changeMessage={setFailureMsg} />}
+            {successMsg && <Messenger message='Your changes were saved.' severity='success' open={true} changeMessage={setSuccessMsg} />}
+            {failureMsg && <Messenger message='Your changes could not be saved.' severity='warning' open={true} changeMessage={setFailureMsg} />}
             <CenterModal show={modalShow}
                 handleClose={() =>
                     setModalShow(false)
