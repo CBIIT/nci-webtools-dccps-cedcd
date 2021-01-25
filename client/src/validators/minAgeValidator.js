b345db119b9d1bf42ce7967ac35ed2456609bb9d
Zhao <zhaox18@nih.gov>
2021-01-25 15:25:13 -0500

    enforced age validation rules

--- client/src/validators/minAgeValidator.js
+++ client/src/validators/minAgeValidator.js
@@ -1,8 +1,9 @@
 const minAgeValidator = (value, isRequired, maxAge, median, mean) => {
     if(isRequired && value === '')
         return 'Required Field'
-    else if(!/^\s*[0-9][0-9]?\s*$/.test(value))
+        else if(+value< 0 || +value >= 1000){
         return 'Invalid age'
+    }
     else if(maxAge && parseInt(value) > parseInt(maxAge))
             return 'Min age is greater than max age'
     else{
