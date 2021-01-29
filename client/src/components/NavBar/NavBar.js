66d4eb35850b62f6b3bd6dbb4151cc49b7855f65
Zhao <zhaox18@nih.gov>
2021-01-29 10:42:10 -0500

    fixing responsive logout issue'

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -6,12 +6,10 @@ import TourBox from "../Tour/TourBox";
 import {resetUser} from "../../reducers/user"
 const NavBar = (props) => {
   const userSession = useSelector(state => state.user);
-  const dispatch = useDispatch()
-  //const [loggedin, setLogin] = useState(false)
-  const logout = async () => {
+
+  const logout = async (e) => {
     // can not use normal 301 response, since session is not properly cleared
-    // setLogin(false)
-    dispatch(resetUser())
+    e.preventDefault()
     const response = await fetch('/api/logout');
     window.location.href = `${await response.json()}?TARGET=${window.location.origin}`;
   }
@@ -108,8 +106,8 @@ const NavBar = (props) => {
                   </div>
                 </li>
               }
-              {Object.keys(userSession).length > 0 ? <>
-                <li><a href='#' onClick={logout}>Log out</a></li>
+              {userSession && userSession.role ? <>
+                <li><a href='#' target="_self" onClick={logout}>Log out</a></li>
               </> : <>
                 <li><a href='/private/external' >External Login</a></li>
                 <li><a href='/private/internal' >NIH Login</a></li>
