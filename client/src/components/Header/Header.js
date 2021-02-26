eb52919517035c669da2a1ca70632d8b11c5f250
Brian Park <park-brian@users.noreply.github.com>
2021-02-26 10:15:46 -0500

    etreat empty object as null

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -28,7 +28,7 @@ export default function Header(props) {
                         </a>
         
                         <div style={{ marginLeft: '10%', marginTop: '20px' }}>
-                            {userSession && <>
+                            {userSession && Object.keys(userSession.length) && <>
                                 <a
                                     className="login-button"
                                     href="#"
