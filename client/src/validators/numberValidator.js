e64653500dd61ee6e13e24d0567c042b3c2a4617
Zhao <zhaox18@nih.gov>
2021-03-16 10:03:09 -0400

    changed validation message in E.1

--- client/src/validators/numberValidator.js
+++ client/src/validators/numberValidator.js
@@ -1,6 +1,6 @@
 const numberValidator = (value, isRequired, allowNegative) =>{
     let pattern = allowNegative ? /^\s*\-?[1-9][0-9]*\s*$/ : /^\s*[1-9][0-9]*\s*$/
-    if(isRequired && !value) return 'Please provide a value'
+    if(isRequired && !value) return 'Required field'
     if(!pattern.test(value)) return 'Invalid value'
 }
 
