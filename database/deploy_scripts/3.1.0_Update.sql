0cb7d729c3ad3e8b17b90456a098674eab089e78
Chao Zhang <chao.zhang3@nih.gov>
2021-05-06 16:15:41 -0400

    test db -update script

--- database/deploy_scripts/3.1.0_Update.sql
+++ database/deploy_scripts/3.1.0_Update.sql
@@ -7,19 +7,7 @@
 */
 
 use cedcd;
--- -----------------------------------------------------------------------------------------------------------
--- View: v_lu_collected_specimen
--- -----------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE VIEW v_lu_collected_specimen AS
-    SELECT 
-        MIN(lu_specimen.id) AS id, lu_specimen.specimen AS specimen
-    FROM lu_specimen
-    WHERE sub_category IS NOT NULL
-        AND specimen IN ('Blood' , 'Buccal/Saliva',
-            'Tissue (includes tumor and/or normal)',
-            'Urine', 'Feces','Other')
-    GROUP BY specimen
-    ORDER BY id;
-
-
+insert into cohort_user_mapping (cohort_acronym,user_id,active)
+Values ('ATBC', 1,'Y'),
+('BCSC',1,'Y');
