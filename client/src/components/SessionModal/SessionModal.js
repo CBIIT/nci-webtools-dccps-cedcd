feb31093ad6f7c4cd24577706cae80e79dc89c59
Brian Park <park-brian@users.noreply.github.com>
2021-05-20 17:02:34 -0400

    update session timeout modal

--- client/src/components/SessionModal/SessionModal.js
+++ client/src/components/SessionModal/SessionModal.js
@@ -1,129 +1,94 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateUserSession, fetchUser } from '../../reducers/user';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 // import './login-button.css'
 
-export default function SessionModal({warningTime}) {
+// note: the warning threshold should be a value under the default session timeout
+// otherwise, you will always see the warning
+export default function SessionModal({showWarningThreshold = 300, checkInterval = 60}) {
     const userSession = useSelector(state => state.user);
     const isLoggedIn = Object.keys(userSession).length > 0;
+    const [remainingTime, setRemainingTime] = useState(getRemainingTime());
+    const [showWarning, setShowWarning] = useState();
     const dispatch = useDispatch();
-    const remainingMs = isLoggedIn
-        ? (userSession.expires - new Date().getTime())
-        : 0;
 
-    if (isLoggedIn && remainingMs < 0) {
-        // window.location.reload();
-    }
-    
-    new Date().getTime()
-    
     useEffect(() => {
-        const interval = setInterval(() => {
-            dispatch(fetchUser());
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
+        // todo: use websockets instead
+        const userTimerInterval = setInterval(updateRemainingTime, 100);
+        const sessionCheckInterval = setInterval(checkUserSession, 1000 * checkInterval);
+        window.addEventListener('storage', syncRemainingTime);
+        window.localStorage.setItem('cedcd.remainingTime', remainingTime);
 
-    const refreshUserSession = () => {
+        return () => {
+            clearInterval(userTimerInterval);
+            clearInterval(sessionCheckInterval);
+            window.removeEventListener('storage', syncRemainingTime);
+        }
+    }, [userSession, checkInterval]);
+
+    function syncRemainingTime(event) {
+        if (event.storageArea != localStorage) return;
+        if (event.key === 'cedcd.remainingTime') {
+            setRemainingTime(Number(event.newValue));
+            checkUserSession();
+        }
+    }
+
+    function updateRemainingTime() {
+        const remaining = getRemainingTime();
+
+        if (isLoggedIn) {
+            setShowWarning(remaining >= 0 && remaining < showWarningThreshold);
+            if (remaining < 0) 
+                checkUserSession();
+        }
+
+        setRemainingTime(remaining);
+    }
+
+    function getRemainingTime() {
+        return isLoggedIn
+            ? (userSession.expires - new Date().getTime()) / 1000
+            : 0;
+    }
+
+    function checkUserSession() {
+        dispatch(fetchUser());
+    }
+
+    function refreshUserSession() {
+        setShowWarning(isLoggedIn && remainingTime < showWarningThreshold);
         dispatch(updateUserSession());
     }
 
     // console.dir('in header: ',userSession)
-    const logout = async e => {
+    async function logout(e) {
         e.preventDefault();
         // can not use normal 301 response, since session is not properly cleared
         const response = await fetch('/api/logout');
         window.location.href = `${await response.json()}?TARGET=${window.location.origin}`;
     }
 
-
-
     return (
-        <div>
-            { remainingMs }
-            <button onClick={refreshUserSession}>Refresh</button>
-            <pre>{JSON.stringify(userSession, null, 2)}</pre>
-
-        </div>
-    )
-
-    /*
-            <div className="d-none d-md-block">
-                <div id="header">
-                    <div id="header-inner" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <a href="/" style={{ border: '0' }}>
-                            <img
-                                src="/assets/img/logo_CEDCD_white.png"
-                                id="ctl09_mastLogo"
-                                alt="Cancer Epidemiology Descriptive Cohort Database"
-                                style={{ marginLeft: '0px', marginTop: '6px' }}
-                            />
-                        </a>
-        
-                        <div style={{ marginLeft: '10%', marginTop: '20px' }}>
-                            {isLoggedIn && <>
-                                <a
-                                    className="login-button"
-                                    href="#"
-                                    onClick={logout}
-                                    style={{ margin: '5px' }}
-                                    target="_self">
-                                    Logout
-                                </a>
-                            </> || <>
-                                    <a
-                                        className="login-button"
-                                        href="/private/external"
-                                        style={{ margin: '5px' }}
-                                        target="_self">
-                                        External Login
-                                </a>
-                                    <a
-                                        className="login-button"
-                                        href="/private/internal"
-                                        style={{ margin: '5px' }}
-                                        target="_self">
-                                        NIH Login
-                                </a>
-                                </>}
-                        </div>
-                    </div>
-                </div>      
-            </div>
-            <div className="d-block d-md-none">
-                <div className="row">
-                    {isLoggedIn && 
-                        <div className="col-12">
-                            <a
-                                className="login-button float-right"
-                                href="#"
-                                onClick={logout}
-                                style={{ margin: '5px' }}
-                                target="_self">
-                                Logout
-                            </a>
-                        </div> 
-                        || 
-                        <div className="col-12">
-                            <a
-                                className="login-button float-right"
-                                href="/private/internal"
-                                style={{ margin: '5px' }}
-                                target="_self">
-                                NIH Login
-                            </a>
-                            <a
-                                className="login-button float-right"
-                                href="/private/external"
-                                style={{ margin: '5px' }}
-                                target="_self">
-                                External Login
-                            </a>
-                        </div>
-                    }
+        <Modal show={isLoggedIn && showWarning} backdrop="static" keyboard={false}>
+            <Modal.Header>
+                <Modal.Title>Warning: Session Timeout</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div class="p-5">
+                    Your session is about to expire. Please select an option below.
                 </div>
-            </div>  
-        </div>
-    )
-    */
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={logout}>
+                Log Out
+            </Button>
+            <Button variant="primary" onClick={refreshUserSession}>
+                Extend My Session
+            </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
