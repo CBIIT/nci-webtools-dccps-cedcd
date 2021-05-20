115ed7379e381cea583ff21d0f73d840f2a8b020
Brian Park <park-brian@users.noreply.github.com>
2021-05-20 15:21:19 -0400

    check if user is logged in and has role

--- client/src/components/RequireAuthorization/RequireAuthorization.js
+++ client/src/components/RequireAuthorization/RequireAuthorization.js
@@ -4,7 +4,10 @@ import Unauthorized from '../Unauthorized/Unauthorized'
 
 export default function RequireAuthorization({role, children}) {
     const user = useSelector(state => state.user);
-    const isAuthorized = user && (!role || user.role === role);
+    const roles = Array.isArray(role) ? role : [role];
+    const isLoggedIn = Object.keys(user).length > 0;
+    const hasRole = !roles.length || roles.includes(user.role);
+    const isAuthorized = isLoggedIn && hasRole;
 
     return isAuthorized
         ? <>{children}</>
