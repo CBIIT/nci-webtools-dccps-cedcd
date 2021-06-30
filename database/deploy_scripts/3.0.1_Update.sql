b37202fff251b9220ec7d9aa72713df7d445fea6
Chao Zhang <chao.zhang3@nih.gov>
2021-06-29 23:08:19 -0400

    DB changes for Section C SS-4144 User Feedback updates for N/A options

--- database/deploy_scripts/3.0.1_Update.sql
+++ database/deploy_scripts/3.0.1_Update.sql
@@ -9,7 +9,7 @@
 use cedcd;
 
 -- add new category as N/A type
-/*
+
 insert ignore into lu_data_category (id, category, sub_category) 
 values (42,'Other Tobacco Products','Not Applicable'); 
 
@@ -19,10 +19,3 @@ from major_content b where b.cohort_id not in (select distinct c.cohort_id from 
 
 -- add new field to keep cancer condition N/A status
 alter table cancer_info add column (mdc_cancer_related_conditions_na int);
-*/
-
-delete from major_content where category_id = 42;
-
-delete from lu_data_category where id=42;
-
-alter table cancer_info drop column mdc_cancer_related_conditions_na ;
