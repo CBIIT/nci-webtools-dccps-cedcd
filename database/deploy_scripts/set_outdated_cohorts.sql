64ff82e4fa0ca798256c2c36f28c5dd6722eddb7
Phyllip Cho <phyllip3@users.noreply.github.com>
2023-11-03 15:28:58 -0400

    set outdated cohorts

--- database/deploy_scripts/set_outdated_cohorts.sql
+++ database/deploy_scripts/set_outdated_cohorts.sql
@@ -1,18 +1,11 @@
 USE cedcd;
+SET sql_safe_updates=0;
 
 DROP PROCEDURE IF EXISTS `?`;
 DELIMITER //
 CREATE PROCEDURE `?`()
 BEGIN
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
-  
-    ALTER TABLE cohort
-    ADD `type` VARCHAR(20) NOT NULL DEFAULT 'Etiology',
-    ADD `outdated` BOOLEAN NOT NULL DEFAULT false;
-
-    UPDATE cohort
-    SET `type` = 'Survivor'
-    WHERE acronym = 'Pathways';
 
     UPDATE cohort
     SET `outdated` = true
@@ -21,3 +14,5 @@ END //
 DELIMITER ;
 CALL `?`();
 DROP PROCEDURE `?`;
+
+SET sql_safe_updates=1;
